fda0e6729407293fb554ec2ca97850a06accd633
mikeylights <michael.h.sanchez@gmail.com>
2013-08-27 23:24:53 -0400

    initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+
+  def home
+  end
+
+  def step2
+  end
+
+  def step3  	
+  end
+
+  def newevent
+  end
+  
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HrManagerProfile::Application.config.secret_token = '2106602708f2b1b817f14b5a311aa3b0d2edb8cc83caceda07d62e4c6e30e5893f1cda614b9591afaae847ed4d64936137c21854db22259a020bbc8416b537d4'
